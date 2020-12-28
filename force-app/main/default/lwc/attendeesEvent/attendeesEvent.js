f684a314034f3d642c8c846b6c18def2a6d54632
Puneet Gaur <puneetgaur@MacBook-Pro.local>
2020-12-27 22:34:21 -0700

    Changes

---
+++ force-app/main/default/lwc/attendeesEvent/attendeesEvent.js
@@ -0,0 +1,57 @@
+import { LightningElement , api, track } from 'lwc';
+import getUpcomingEvents from '@salesforce/apex/AttendeeEventService.getUpcomingEvents';
+const columns = [
+    {
+        label: "Event Name",
+        fieldName: "detailPage",
+        type: "url",
+        wrapText: "true",
+        typeAttributes:{
+            label:{
+                fieldName : "Name__c"
+            },
+            target: "_blank"
+        }
+    },
+    {   
+        label: 'Name', 
+        fieldName: 'EVNTORG',
+        cellAttributes:{
+            iconName: 'standard:event',
+            iconPosition: 'right'
+        }
+    },
+    { label: 'Event Date', fieldName: 'StartDateTime', type: 'datetime' },
+    { label: 'Location', fieldName: 'Location', type: 'text' }
+
+];
+export default class AttendeesEvent extends LightningElement {
+    @api recordId;
+    @track events;
+    errors;
+    columnList = columns;
+    connectedCallback(){
+        this.upcomingEventList();
+    }
+    upcomingEventList(){
+        getUpcomingEvents({ attendeeId: this.recordId })
+        .then(result => {
+            result.forEach(attendee => {
+                attendee.Name = attendee.Event__r.Name;
+                attendee.detailPage = "https://"+window.location.host+'/'+attendee.Event__c;
+                attendee.EVNTORG = attendee.Event__r.Event_Organizer__r.Name;
+                attendee.StartDateTime = attendee.Event__r.Start_Date_Time__c;
+                if(attendee.Event__r.Location__c){
+                    attendee.Location = attendee.Event__r.Location__r.Name;
+                }else{
+                    attendee.Location = 'Not Defined- Virtual Event Defined';
+                }
+            });
+            this.events = result;
+        })
+        .catch(error => {
+            this.events = undefined;
+            this.errors = JSON.stringify(error);
+        });
+    }
+}
